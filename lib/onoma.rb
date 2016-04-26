e68326aea249f9d3f73ac47c6b88b2a265d745dd
Brice TEXIER <brice@ekylibre.com>
2016-04-26 22:26:09 +0200

    Updates nomenclatures data

--- lib/onoma.rb
+++ lib/onoma.rb
@@ -79,7 +79,7 @@ module Onoma
     def find(*args)
       options = args.extract_options!
       name = args.shift
-      if args.size == 0
+      if args.empty?
         return @@set[name]
       elsif args.size == 1
         return @@set[name].find(args.shift) if @@set[name]
